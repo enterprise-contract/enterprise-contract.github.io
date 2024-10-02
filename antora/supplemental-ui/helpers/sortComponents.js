9c8ebf7b57fd6c8a24877b1a7edb894961c15b3c
Simon Baird <sbaird@redhat.com>
2024-10-02 17:58:44 -0400

    Support ordering the components in the nav menu
    Requires some javascript since it appears the order of the
    components in `site.components` is not based on how they're listed
    in the playbook file.
    
    I'm not really sure how the order was determined previously, but
    after this patch we can definitely control it.
    
    Note that #each works roughly the same for an object and a list so
    that's why there's not much changing in the partial.
    
    Ref: https://issues.redhat.com/browse/EC-903

---
+++ antora/supplemental-ui/helpers/sortComponents.js
@@ -0,0 +1,23 @@
+// Used in the nav-menu.hbs partial to specify component order
+
+const objectValues = function (obj) {
+  return Object.keys(obj).map((k) => obj[k])
+}
+
+const componentOrder = [
+  "ec",
+  "ec-cli",
+  "ecc",
+  "ec-policies",
+  "user-guide",
+]
+
+const componentSorter = function (a, b) {
+  const aa = componentOrder.indexOf(a.name)
+  const bb = componentOrder.indexOf(b.name)
+  return (aa > bb ? 1 : (aa < bb ? -1 : 0))
+}
+
+module.exports = (components) => {
+  return objectValues(components).sort(componentSorter)
+}
